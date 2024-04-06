70419a50d8b9abb649d09240fa059461fa323da3
MajesticNthg <majestic_nthg@mail.ru>
2024-04-06 16:55:19 +1000

    mod.1

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,13 @@
+
+=
+src/Main.java,9\2\924f089edb4af5ee45c8a9d1129cd28e2b368138
+?
+src/Level1.java,0\7\07084fabeefc82b5120f1af8a8f1b29bd1b8c52a
+Å
+Q.idea/sonarlint/securityhotspotstore/0/7/07084fabeefc82b5120f1af8a8f1b29bd1b8c52a,3\7\37ff92c265db56e65336b84efedd78846e98d30e
+]
+-.idea/sonarlint/securityhotspotstore/index.pb,8\8\88a9255124c95bdc913197c120a8d560edc59c8e
+Å
+Q.idea/sonarlint/securityhotspotstore/9/2/924f089edb4af5ee45c8a9d1129cd28e2b368138,b\0\b08f1ab659fbdd998f45c0def99188f980cc899b
+S
+#.idea/sonarlint/issuestore/index.pb,9\f\9fe84ebb15faf917b7def6236dba604453cc61e0
--- src/Level1.java
+++ src/Level1.java
@@ -1,9 +1,39 @@
 public class Level1 {
-    public static void main(String[] args) {
-        int x = 2;
-        int y = 4;
+    public static int[] Eight (int N, int[] data) {
+        int i = 0;
+        int[] result = new int[N];
 
-        System.out.println(Math.pow(2, 4));
+        for (int x = 0; x < N; x++) {
+            for (int y = 0; data[x] > 0; y++) {
+                i += (int)( (data[x] % 10) * Math.pow(8, y));
+                data[x] /= 10;
+            }
+            result[x] = i;
+            i = 0;
+        }
+        return result;
+       }
 
-    }
+       public static int[] Sixteen (int N, int[] data) {
+        int i = 0;
+        int[] result = new int[N];
+
+           for (int x = 0; x < N; x++) {
+               for (int y = 0; data[x] > 0; y++) {
+                   i += (int)( (data[x] % 10) * Math.pow(16, y));
+                   data[x] /= 10;
+               }
+               result[x] = i;
+               i = 0;
+           }
+           return result;
+       }
+
+       public static int [] UFO (int N, int [] data, boolean octal) {
+        if (octal)
+            return Eight(N, data);
+        return Sixteen(N, data);
+       }
+
 }
+
