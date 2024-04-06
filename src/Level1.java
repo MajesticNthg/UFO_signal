dc8a46eae4df90e6c6c497c0a9183405041babac
MajesticNthg <majestic_nthg@mail.ru>
2024-04-06 22:15:27 +1000

    mod.3.1

--- src/Level1.java
+++ src/Level1.java
@@ -1,24 +1,23 @@
 public class Level1 {
- public static int [] UFO (int N, int [] data, boolean octal) {
+    public static int[] UFO(int N, int[] data, boolean octal) {
         int z = 16;
         if (octal) {
             z = 8;
         }
 
+        int i = 0;
+        int[] result = new int[N];
 
-           int i = 0;
-           int[] result = new int[N];
-
-           for (int x = 0; x < N; x++) {
-               for (int y = 0; data[x] > 0; y++) {
-                   i += (int)( (data[x] % 10) * Math.pow(z, y));
-                   data[x] /= 10;
-               }
-               result[x] = i;
-               i = 0;
-           }
-           return result;
-       }
+        for (int x = 0; x < N; x++) {
+            for (int y = 0; data[x] > 0; y++) {
+                i += (int) ((data[x] % 10) * Math.pow(z, y));
+                data[x] /= 10;
+            }
+            result[x] = i;
+            i = 0;
+        }
+        return result;
+    }
 
 }
 
